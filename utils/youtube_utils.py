afe05ee6e953ba66ef5ba76b1908f3a9a7a48d68
Faisal Alharbi <FaisalGAlharbi@outlook.com>
2025-05-11 22:02:44 +0300

    Update youtube_utils.py

--- utils/youtube_utils.py
+++ utils/youtube_utils.py
@@ -1,5 +1,8 @@
 from urllib.parse import urlparse, parse_qs
 import yt_dlp
+import os
+from yt_dlp import YoutubeDL
+
 
 def extract_video_id(youtube_url):
     query = urlparse(youtube_url)
@@ -10,10 +13,16 @@ def extract_video_id(youtube_url):
     return None
 
 def download_audio(youtube_url, output_path):
+    proxy = os.getenv("PROXY_URL")
+
     ydl_opts = {
         'format': 'bestaudio[ext=webm]',
         'outtmpl': output_path,
-        'quiet': False
+        'quiet': False,
     }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
+    if proxy:
+        ydl_opts['proxy'] = proxy  # ✅ إضافة البروكسي من .env
+
+    with YoutubeDL(ydl_opts) as ydl:
         ydl.download([youtube_url])
